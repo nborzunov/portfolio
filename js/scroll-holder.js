bc698af7d8f520a4bdbd864660d17ecec2cf8597
unknown <forest3694@ya.ru>
2020-09-03 16:56:09 +0300

    fixed header

---
+++ js/scroll-holder.js
@@ -0,0 +1,8 @@
+// window.onscroll = function showHeader() {
+//     var header = document.querySelector('.navbar');
+//     if(window.pageYOffset > 80){
+//         header.classList.add('navbar_fixed');
+//     } else{
+//         header.classList.remove('navbar_fixed');
+//     }
+// }
